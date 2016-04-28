032be972ab2c2e5ea41f44a9883d352103310ee5
Walter Sagehorn <w.sagehorn@gmail.com>
2016-04-28 02:45:03 -0500

    website exists, no content

--- my_app.py
+++ my_app.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, session, redirect, request, url_for
 from flask.ext.mobility import Mobility
 from flask.ext.mobility.decorators import mobile_template
 
-from forms import NameForm
-
 app = Flask(__name__)
 Mobility(app)
 
@@ -13,17 +11,7 @@ app.secret_key = 'CHANGE_ME'
 @app.route('/', methods=['GET','POST'])
 @mobile_template('{mobile/}index.html')
 def index(template):
-	form = NameForm(request.form)
-	if request.method == 'POST' and form.validate():
-		session["name"] = form.name.data
-	return render_template(template, form=form)
-
-#clears the session and then redirects back to index
-@app.route('/logout')
-def logout():
-	session["name"] = ""
-	return redirect(url_for('index'))
-
+	return render_template(template)
 
 if __name__ == "__main__":
     app.run(debug=True)
