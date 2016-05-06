fb71480d772edadd16ef3fe9d0309a19b986872a
Walter Sagehorn <w.sagehorn@gmail.com>
2016-05-05 20:00:50 -0500

    giving up on SSL

--- start_server.py
+++ start_server.py
@@ -5,17 +5,18 @@ from tornado.ioloop import IOLoop
 
 from my_app import app
 
-try:
-    http_server = HTTPServer(WSGIContainer(app), ssl_options={
-        "certfile": "/var/www/cert/domain.crt",
-        "keyfile": "/var/www/cert/domain.key",
-    })
-    http_server.listen(443)
+#try:
+#    http_server = HTTPServer(WSGIContainer(app), ssl_options={
+#        "certfile": "/var/www/cert/domain.crt",
+#        "keyfile": "/var/www/cert/domain.key",
+#    })
+#    http_server.listen(443)
+#
+#except ValueError:
+#    print "can't find certificate, starting on port 8080 instead"
+#    http_server = HTTPServer(WSGIContainer(app))
+#    http_server.listen(8080)
 
-except ValueError:
-    print "can't find certificate, starting on port 8080 instead"
-    http_server = HTTPServer(WSGIContainer(app))
-    http_server.listen(8080)
-
-
+http_server = HTTPServer(WSGIContainer(app))
+http_server.listen(80)
 IOLoop.instance().start()
